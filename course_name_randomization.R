efa41737c2bf113054e9ddb503f9fd757c4cf091
nora-lvr <livestockvets@gmail.com>
2026-06-02 09:51:14 -0500

    added randomized names

---
+++ course_name_randomization.R
@@ -0,0 +1,14 @@
+class<-c('Bailey',  
+         'Danielle', 
+         'Blaine', 
+         'Allison',
+         'Acir', 
+         'Sara', 
+         'Michelle', 
+         'Mara',
+         'Shankar',
+         'Rafael',
+         'Thiago') 
+
+sample(class)
+
